8677e2993b1a3d6205a97456586223cdad7fd383
ad222kr <ad222kr@student.lnu.se>
2016-01-27 15:34:39 +0100

    Login tests. Working on remember me

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  root                   'home#index'
-  get 'signup'      =>   'users#new'
+
+  root                        'home#index'
+  get      'signup'      =>   'users#new'
+  get      'login'       =>   'sessions#new'
+  post     'login'       =>   'sessions#create'
+  delete   'logout'      =>   'sessions#destroy'
 
   resources :users
 
