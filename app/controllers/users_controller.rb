4b773b95ce38089349e2c1f5f8405ba00b95f499
ad222kr <ad222kr@student.lnu.se>
2016-02-22 19:02:30 +0100

    Started working with the API. Namespace for api and versioning started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,8 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
+    
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
 
-
   root                        'home#index'
   get      'signup'      =>   'users#new'
   get      'login'       =>   'sessions#new'
   post     'login'       =>   'sessions#create'
   delete   'logout'      =>   'sessions#destroy'
 
-  # resources :users do
-  #   resources :apps, only: [:new, :create, :destroy]
-  # end
-
   resources :apps, only: [:new, :create, :destroy]
   resources :users, except: [:index, :destroy]
+
+  namespace :api, defaults: { format: :json },
+                            constraints: { subdomain: 'api' }, path: '/' do
+
+  end
 
 
 end
