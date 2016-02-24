c25e565925d33335036cc2382532e32ac10ca899
ad222kr <ad222kr@student.lnu.se>
2016-02-24 12:26:58 +0100

    Get json-response of a pub

--- app/controllers/api/v1/pubs_controller.rb
+++ app/controllers/api/v1/pubs_controller.rb
@@ -1,8 +1,24 @@
 class Api::V1::PubsController < ApplicationController
+  protect_from_forgery with: :null_session
   respond_to :json
 
   def show
     respond_with Pub.find(params[:id])
   end
+
+  def create
+    pub = Pub.new(pub_params)
+    if pub.save
+      render json: pub, status: 201, location: [:api, pub]
+    else
+      render json: { errors: pub.errors }, status: 422
+    end
+
+  end
+
+  private
+    def pub_params
+      params.require(:pub).permit(:name, :phone_number, :description)
+    end
 
 end
