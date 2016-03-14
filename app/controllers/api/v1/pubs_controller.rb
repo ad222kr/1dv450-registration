0d761c39ab50c0cd76f16066ad99f82385b44217
Alex <ad222kr@student.lnu.se>
2016-03-14 01:13:03 +0100

    can add pubs with location and tags and some error-checking in controller-layer

--- app/controllers/api/v1/pubs_controller.rb
+++ app/controllers/api/v1/pubs_controller.rb
@@ -15,17 +15,44 @@ class Api::V1::PubsController < Api::V1::ApiBaseController
   end
 
   def create
-    pub = Pub.new(pub_params)
+
+    if pub_params[:position].blank?
+      render json: { errors: "The pub needs aposition!" }
+      return
+    end
+
+    if Position.find_by_address(pub_params[:position][:address])
+      render json: { errors: "There already exists a pub at that location. Cant have two pubs at the same location eh?" }, status: :conflict
+      return
+    end
+
+
+
+    pub = Pub.new(pub_params.except(:tags, :position))
     pub.creator = current_user
+    pub.position = Position.create(pub_params[:position])
+
+
+
+
+    if tags = pub_params[:tags]
+      tags.each do |tag|
+        pub.tags << Tag.where(tag).first_or_create
+      end
+    end
+
     if pub.save
       render json: pub, status: 201, location: [:api, pub]
     else
-      render json: { errors: pub.errors }, status: 422
+      render json: { errors: pub.errors }, status: :conflict
     end
   end
 
   def update
     pub = Pub.find(params[:id])
+    # render json: { "developer_error": "Could not find a pub with that id", "user_error": "Something went wrong" } unless pub
+
+
     if pub.update(pub_params)
       render json: pub, status: 200, location: [:api, pub]
     else
@@ -41,9 +68,7 @@ class Api::V1::PubsController < Api::V1::ApiBaseController
 
   private
     def pub_params
-      params.require(:pub).permit(:name, :phone_number, :description)
+      json_params = ActionController::Parameters.new(JSON.parse(request.body.read))
+      json_params.require(:pub).permit(:name, :phone_number, :description, tags: [:name], position: [:address])
     end
-
-
-
 end
