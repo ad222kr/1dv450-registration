41f704a209711dbc6e4d8f592960d884a0a4bf89
Alex <ad222kr@student.lnu.se>
2016-03-14 02:30:17 +0100

    can update a pub with PUT

--- app/controllers/api/v1/pubs_controller.rb
+++ app/controllers/api/v1/pubs_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::PubsController < Api::V1::ApiBaseController
 
+
   skip_before_action :authenticate, only: [:index, :show]
 
   respond_to :json
@@ -10,30 +11,18 @@ class Api::V1::PubsController < Api::V1::ApiBaseController
 
 
   def show
-    @pub = Pub.find(params[:id])
-    respond_with @pub, status: :ok
+    pub = Pub.find(params[:id])
+    respond_with pub, status: :ok
   end
 
+
+
   def create
-
-    if pub_params[:position].blank?
-      render json: { errors: "The pub needs aposition!" }
-      return
-    end
-
-    if Position.find_by_address(pub_params[:position][:address])
-      render json: { errors: "There already exists a pub at that location. Cant have two pubs at the same location eh?" }, status: :conflict
-      return
-    end
-
-
+    return unless pub_has_position? && position_is_available?
 
     pub = Pub.new(pub_params.except(:tags, :position))
     pub.creator = current_user
     pub.position = Position.create(pub_params[:position])
-
-
-
 
     if tags = pub_params[:tags]
       tags.each do |tag|
@@ -44,19 +33,22 @@ class Api::V1::PubsController < Api::V1::ApiBaseController
     if pub.save
       render json: pub, status: 201, location: [:api, pub]
     else
-      render json: { errors: pub.errors }, status: :conflict
+      render json: { errors: pub.errors.messages }, status: 402
     end
   end
 
+
   def update
-    pub = Pub.find(params[:id])
-    # render json: { "developer_error": "Could not find a pub with that id", "user_error": "Something went wrong" } unless pub
+    pub = Pub.find_by_id(params[:id])
+    render json: { error: "You did not create the post and cannot edit it" } and return unless current_user == pub.creator
+
+    render json: { error: "Could not find a pub with that id"} unless pub
 
 
     if pub.update(pub_params)
-      render json: pub, status: 200, location: [:api, pub]
+      render json: { action: "update", pub: PubSerializer.new(pub) }, status: :ok
     else
-
+      render json: { errors: pub.errors.messages }, status: :bad_request
     end
   end
 
@@ -70,5 +62,21 @@ class Api::V1::PubsController < Api::V1::ApiBaseController
     def pub_params
       json_params = ActionController::Parameters.new(JSON.parse(request.body.read))
       json_params.require(:pub).permit(:name, :phone_number, :description, tags: [:name], position: [:address])
+    end
+
+    def pub_has_position?
+      if pub_params[:position].blank?
+        render json: { errors: "The pub needs aposition!" }
+        return false
+      end
+      return true
+    end
+
+    def position_is_available?
+      if Position.find_by_address(pub_params[:position][:address])
+        render json: { errors: "There already exists a pub at that location. Cant have two pubs at the same location eh?" }, status: :conflict
+        return false
+      end
+      return true
     end
 end
--- app/models/pub.rb
+++ app/models/pub.rb
@@ -1,12 +1,11 @@
 class Pub < ActiveRecord::Base
-  include Rails.application.routes.url_helpers
 
   belongs_to :creator
   belongs_to :position
   has_and_belongs_to_many :tags
 
   validates :name, presence: true
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true, uniqueness: true
   validates :description, presence: true
 
 end
